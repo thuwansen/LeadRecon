41d5bbe4a0a7c27a884e9ead0380da6f8cb1c6aa
WanSen <wansen@aliyun.com>
2016-08-24 16:55:29 +0800

    modify normalise

--- LeadReconCode/normalise_nonlinear.m
+++ LeadReconCode/normalise_nonlinear.m
@@ -5,8 +5,7 @@ function normalise_nonlinear(fixPath,movePath,fixImg,moveImg,normMat)
 % function: move the moveImg to the fixImg
 % output: w_moveImg is the final  image for use
 % ex. normalise_nonlinear('./templates/','./TestData/','avg152T2.nii','anat.nii','norm.mat')
-% 2016.07.22 Wansen
+% 2016.08.22 Wansen
 
 spm_normalise([fixPath,fixImg],[movePath,moveImg],normMat);
 spm_write_sn([movePath,moveImg], normMat);
-movefile(normMat,movePath);
